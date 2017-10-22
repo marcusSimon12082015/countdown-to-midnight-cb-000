7f3984f2ccbc3480d11d5401b010bf52c4c2d546
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-22 09:41:32 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,6 +5,7 @@ def countdown(number)
     puts "#{number} SECOND(S)!"
     number -= 1
   end
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
@@ -13,4 +14,5 @@ def countdown_with_sleep(number)
     number -= 1
     sleep(1)
   end
+  return "HAPPY NEW YEAR!"
 end
